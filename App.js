f7fbe46c0ed8e785852f965bf491d643aaedf3c4
unknown <rodrigo.domingues.87@hotmail.com>
2022-04-14 15:29:32 -0300

    limpesa da app.js

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 
 import Cesta from './src/telas/Cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
+      <StatusBar/>
       <Cesta/>
-      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

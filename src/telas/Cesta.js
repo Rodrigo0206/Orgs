93b55663336d297589b6b650138bdc98e3a0d62a
unknown <rodrigo.domingues.87@hotmail.com>
2022-04-22 10:34:15 -0300

    dimenssoes

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image, Dimensions } from 'react-native';
 
 import topo from '../../assets/topo.png';
 
 export default function Cesta (){
-    return <Image source={topo} />
+    return <Image source={topo} style={estilos.topo} />
 }
 
 const estilos = StyleSheet.create({
     topo: {
         width: "100%",
+        height: 200,
     }
 });

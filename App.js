7a6ac8b020f7fc76353f49e22777de8cbcdb3050
unknown <rodrigo.domingues.87@hotmail.com>
2022-04-27 12:13:39 -0300

    fonte montSerrat

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { StatusBar, SafeAreaView } from 'react-native';
+import { 
+  useFonts, 
+  Montserrat_400Regular, 
+  Montserrat_700Bold 
+} from '@expo-google-fonts/montserrat';
 
 import Cesta from './src/telas/Cesta';
 
